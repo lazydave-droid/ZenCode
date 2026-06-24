512aebc2cad3257a54be27babbd0ac04d999f656
DS Webhosting <support@dswebhosting.net.au>
2026-06-24 18:45:59 +1000

    Create run_zen.sh

---
+++ run_zen.sh
@@ -0,0 +1,8 @@
+# Run with default local Ollama server
+./run_zen.sh
+
+# Run with remote Ollama server
+./run_zen.sh --ollama-url http://your-remote-server:11434
+
+# Run with custom port
+./run_zen.sh --ollama-url http://localhost:11435
